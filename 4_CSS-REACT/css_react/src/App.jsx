e5d9d3bc19d5f9e2fdeab1575774faea3f55c34c
mcscode <mcs@mcscode.mcscode.com.br>
2023-05-30 20:37:16 -0300

    css inline dinamico

--- 4_CSS-REACT/css_react/src/App.jsx
+++ 4_CSS-REACT/css_react/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import MyComponent from './components/MyComponent'
 
 function App() {
+  const n = 5
+  const name = 'Marcio'
 
   return (
     <>
@@ -12,6 +14,9 @@ function App() {
       <MyComponent />
       {/**Css Inline */}
       <p style={{color:"red", backgroundColor:"gray", borderTop:"solid 2px yellow", padding:"5px"}}>Este é um css inline</p>
+      {/**Css inline dinamico co if ternario */}
+      <h3 style={n > 10 ? {backgroundColor:'black', color:'green'}:{color:'purple'}}>Css Inline dinamico</h3>
+      <h3 style={name === 'Marcio' ? {backgroundColor:'blue', color:'white', borderRadius:'10px'}:{color:'purple'}}>Nome: {name}</h3>
     </>
   )
 }
