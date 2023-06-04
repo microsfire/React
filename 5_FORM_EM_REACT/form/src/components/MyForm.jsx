74b96bd3c28aebaaf6e44732a1e0d326fbd962d4
mcscoders <mcscode@outlook.com.br>
2023-06-04 19:00:49 -0300

    capturando dados do input

--- 5_FORM_EM_REACT/form/src/components/MyForm.jsx
+++ 5_FORM_EM_REACT/form/src/components/MyForm.jsx
@@ -1,14 +1,28 @@
+import { useState } from 'react'
 import './MyForm.css'
 
 const MyForm = () => {
+    // 3-gerenciamento de dados
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
+    const handleName =(e) => {
+        setName(e.target.value)
+    }
+    console.log(name)
   return (
     <div>
-        {/*Criação de form */}
+        {/* 1-criação de form */}
         <form>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type='text' name='name' placeholder='Digite seu nome'/>
+                <input type='text' name='name' placeholder='Digite seu nome' onChange={handleName}/>
             </div>
+            {/* 2-label envolvendo o input */}
+            <label>
+                <span>E-mail:</span>
+                <input type="email" name='email' placeholder='Digite seu email' />
+            </label>
             <input type="submit" value="Enviar"/>
         </form>
     </div>
