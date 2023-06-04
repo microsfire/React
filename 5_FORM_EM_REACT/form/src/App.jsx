43a2b79f2971217297f56e6d804c7bfaa362c62f
mcscoders <mcscode@outlook.com.br>
2023-06-04 18:14:38 -0300

    form

---
+++ 5_FORM_EM_REACT/form/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import MyForm from './components/MyForm'
+
+function App() {
+
+  return (
+    <>
+      <h1>Forms</h1> 
+      <MyForm />
+    </>
+  )
+}
+
+export default App
