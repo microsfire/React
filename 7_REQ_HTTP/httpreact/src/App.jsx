b54ff6d98e913206088d54de14e2ec7d9270d6a8
mcscoders <mcscode@outlook.com.br>
2023-06-26 18:53:14 -0300

    regatando e adicionando dados

--- 7_REQ_HTTP/httpreact/src/App.jsx
+++ 7_REQ_HTTP/httpreact/src/App.jsx
@@ -6,8 +6,12 @@ import { useState, useEffect } from 'react'
 const url = "http://localhost:3000/products"
 
 const App = () => {
+  // states
   const[products, setProducts] = useState([])
-  // 1° resgatando dados
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+
+  // 1° resgatando dados GET
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(url)
@@ -16,7 +20,22 @@ const App = () => {
     }
     fetchData()
   },[])
-  console.log(products)
+  // 2° add produtos POST
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price,
+    }
+    const res = await fetch(url,{
+      method: "POST",
+      headers:{
+        "Content-Type":"application/json",
+      },
+      body: JSON.stringify(product)
+    })
+  }
 
   return (
     <>
@@ -26,6 +45,19 @@ const App = () => {
             <li key={product.id}>{product.name} - R$: {product.price}</li>
           ))}
         </ul>
+        <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+           <label>
+            Preço:
+            <input type="number" name="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+        </div>
     </>
   )
 }
