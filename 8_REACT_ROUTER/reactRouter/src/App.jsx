0d97fdbd23725adafe59063aa688936f494fe148
mcscoders <mcscode@outlook.com.br>
2023-07-11 21:29:29 -0300

    react router, link, navlink, no match route

--- 8_REACT_ROUTER/reactRouter/src/App.jsx
+++ 8_REACT_ROUTER/reactRouter/src/App.jsx
@@ -4,13 +4,16 @@ import './App.css'
 // 1 config react router
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
+//Components
+import Navbar from './components/Navbar'
+
 //Pages
 import Home from './pages/Home'
 import About from './pages/About'
 import Product from './pages/Product'
+import Info from './pages/Info'
+import NotFound from './pages/NotFound'
 
-//Components
-import Navbar from './components/Navbar'
 
 
 function App() {
@@ -22,10 +25,13 @@ function App() {
           <Navbar />
           <Routes>
             {/** Links com react-router */}
-            <Route path='/' element={<Home />}/>
-            <Route path='/about' element={<About />}/>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
             {/**Rota dinamica */}
-            <Route path='/products/:id' element={<Product />}/>
+            <Route path='/products/:id' element={<Product />} />
+            <Route path='/products/:id/info' element={<Info />} />
+            {/** 7 - no match route */}
+            <Route path='*' element={<NotFound />} />
           </Routes>
         </BrowserRouter>
     </div>
--- 8_REACT_ROUTER/reactRouter/src/components/Navbar.jsx
+++ 8_REACT_ROUTER/reactRouter/src/components/Navbar.jsx
@@ -1,13 +1,20 @@
 import './Navbar.css'
 
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 // 1 Links com react-router
 const Navbar = () => {
   return (
     <nav>
+      {/*
         <Link to='/'>Home</Link>
         <Link to='/about'>Sobre</Link>
+  */}
+        {/** NavLink */}
+        <NavLink to='/'
+        //</nav> className={({isActive}) => (isActive ? 'esta-ativo':'nao-ativo')}
+        >Home</NavLink>
+        <NavLink to='/about'>Sobre</NavLink>
     </nav>
   )
 }
