22a433afe6e3cfbc4043c8be7930329efd69339c
mcscoders <mcscode@outlook.com.br>
2023-07-18 21:00:16 -0300

    Context mais complexo usando useReducer

--- 9_CONTEXT/context/src/main.jsx
+++ 9_CONTEXT/context/src/main.jsx
@@ -6,11 +6,17 @@ import './index.css'
 // 2 - criando provider
 import { CounterContextProvider } from './context/CounterContext.jsx'
 
+// 5 - context mais complexo
+import { TitleColorContextProvider } from './context/TitleColorContext.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/** 2 - criando provider */}
     <CounterContextProvider>
-      <App />
+      {/** 5 - context mai complexo */}
+      <TitleColorContextProvider>
+        <App />
+      </TitleColorContextProvider>
     </CounterContextProvider>
   </React.StrictMode>,
 )
--- 9_CONTEXT/context/src/pages/Home.jsx
+++ 9_CONTEXT/context/src/pages/Home.jsx
@@ -1,20 +1,42 @@
-import { useContext } from "react"
-import { CounterContext } from "../context/CounterContext"
+//import { useContext } from "react"
+//import { CounterContext } from "../context/CounterContext"
 
 // 3 - alterando context
 import ChangeCounter from "../components/ChangeCounter"
 
+// 4 - refatorando com hook
+import { useCounterContext } from "../hooks/useCounterContext"
+
+// 5 - context mais complexo
+import { useTitleColorContext } from "../hooks/useTitleColorContext"
 
 const Home = () => {
 
-  const { counter } = useContext(CounterContext)
+  //const { counter } = useContext(CounterContext)
+  const { counter } = useCounterContext()
+
+  // 5 - context mais complexo
+  const { color, dispatch } = useTitleColorContext()
+
+  // 6 - alterando context complexo
+  const setTileColor = (color) => {
+    dispatch({ type: color })
+  }
+
 
   return (
     <div>
-      <h1>Home</h1>
+      <h1 style={{color:color}}>Home</h1>
       <p>Valor do contador: {counter}</p>
       {/** 3 - alterando context */}
       <ChangeCounter />
+      {/** 6 - alterando context complexo */}
+      <div>
+        <button onClick={() => setTileColor("RED")}>Vermelho</button>
+        <button onClick={() => setTileColor("BLUE")}>Azul</button>
+        <button onClick={() => setTileColor("GREEN")}>Verde</button>
+
+      </div>
     </div>
   )
 }
